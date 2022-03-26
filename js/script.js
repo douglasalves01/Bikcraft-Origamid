7b28f5460720a9989b9d190b53e21842520acb72
Douglas Alves <douglasmonttozo@hotmail.com.br>
2022-03-26 00:47:53 -0300

    alterações script

--- js/script.js
+++ js/script.js
@@ -3,9 +3,41 @@ const links = document.querySelectorAll(".header-menu a");
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
-  if (url.includes(href)) {
-    link.classlist.add("ativo");
-  }
+  //if (url.includes(href)) {
+    //link.classlist.add("ativo");
+  //}
 }
 
 links.forEach(ativarLink);
+
+//ativar itens orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+
+////parte das perguntas no seguro
+
+const perguntas =document.querySelectorAll(".perguntas button");
+function ativarPergunta(event){
+  const pergunta= event.currentTarget
+  const controls=pergunta.getAttribute('aria-controls')
+  const resposta=document.getElementById(controls)
+
+  resposta.classList.toggle("ativa")
+  const ativa=resposta.classList.contains("ativa")
+  pergunta.setAttribute("aria-expanded" ,"true")
+
+}
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click',ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
